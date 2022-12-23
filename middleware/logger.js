c2903920a3124a80bca02c49d44642c02e3aa0ce
Firewolf18 <anilkumarakhila80@gmail.com>
2022-12-23 16:31:54 +0530

    cosfg

---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+module.exports=(req,res,next)=>{
+    // if(process.env.NODE_ENV !== 'test'){
+    //     next()
+    // }
+    res.on("finish",()=>{
+        console.log("request log",req.method,req.url,res.statusCode)
+    })
+    next()
+}
